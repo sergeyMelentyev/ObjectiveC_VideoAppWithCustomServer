848af49cc957bc9c59b68ca862edfe72073099e0
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-09 16:53:55 +0300

    new version

--- server/main.js
+++ server/main.js
@@ -6,3 +6,17 @@
 // sudo npm install --save body-parser
 // sudo npm install -g nodemon
 
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+// CREATE A MIDDLEWARE FOR ALL REQUESTS
+app.all("/*", function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With", "Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "POST, GET");
+	next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
