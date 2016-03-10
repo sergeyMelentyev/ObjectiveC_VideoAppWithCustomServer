4015ec2c13db8d8a5b6a9c9fbca01181a971fdc9
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-10 11:51:42 +0300

    new version

---
+++ ShowBestTutsFromWeb/ShowBestTutsFromWeb/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  ShowBestTutsFromWeb
+//
+//  Created by Админ on 10.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
---
+++ ShowBestTutsFromWeb/ShowBestTutsFromWeb/VideoCell.m
@@ -0,0 +1,29 @@
+//
+//  VideoCell.m
+//  ShowBestTutsFromWeb
+//
+//  Created by Админ on 10.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import "VideoCell.h"
+
+@interface VideoCell()
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+@property (weak, nonatomic) IBOutlet UILabel *descripLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbImg;
+@property (weak, nonatomic) IBOutlet UIView *videoCellConteiner;
+@end
+
+@implementation VideoCell
+-(void) awakeFromNib {
+    self.layer.cornerRadius = 2.0;
+    self.layer.shadowColor = [UIColor colorWithRed:157.0/255.0 green:157.0/255.0 blue:157.0/255.0 alpha:0.8].CGColor;
+    self.layer.shadowOpacity = 0.8;
+    self.layer.shadowRadius = 5.0;
+    self.layer.shadowOffset = CGSizeMake(0.0, 2.0);
+}
+-(void) setSelected:(BOOL)selected animated:(BOOL)animated{
+    [super setSelected:selected animated:animated];
+}
+@end
