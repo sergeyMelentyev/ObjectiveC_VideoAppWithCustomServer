5bd96cadb25ce23a8a052ea1e54c4045546c7697
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-24 16:41:12 +0300

    new version

--- server/main.js
+++ server/main.js
@@ -24,17 +24,59 @@ app.use(bodyParser.urlencoded({extended: false}));
 var tutorials = [
 	{
 		id: 1,
-		videoTitle: "Any name here",
-		videoDescription: "Any description here",
+		videoTitle: "[Objective]-C",
+		videoDescription: "General-purpose, object-oriented programming language that adds Smalltalk-style messaging to the C programming language",
 		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/5esQqZIJ83g" frameborder="0" allowfullscreen></iframe></div>',
-		thumbnailUrl: "https://i.ytimg.com/vi/4Zs0gUJ7eAM/maxresdefault.jpg"
+		thumbnailUrl: "https://hsto.org/storage2/679/58d/a41/67958da41af346027a93e0cb37076510.png"
 	},
 	{
 		id: 2,
-		videoTitle: "Any new name here",
-		videoDescription: "Any new description here",
-		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/5esQqZIJ83g" frameborder="0" allowfullscreen></iframe></div>',
-		thumbnailUrl: "https://i.ytimg.com/vi/4Zs0gUJ7eAM/maxresdefault.jpg"
+		videoTitle: "Swift",
+		videoDescription: "General-purpose, multi-paradigm, compiled programming language created for iOS, OS X, watchOS, tvOS and Linux",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/c8BGQ3CfPBs" frameborder="0" allowfullscreen></iframe> </iframe></div>',
+		thumbnailUrl: "http://www.themobilemontage.com/wp-content/uploads/2015/05/apple-swift-logo-icon2.png"
+	},
+	{
+		id: 3,
+		videoTitle: "C",
+		videoDescription: "General-purpose, imperative computer programming language, supporting structured programming, lexical variable scope and recursion",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/de2Hsvxaf8M" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "http://www.pskills.org/image/c.png"
+	},
+	{
+		id: 4,
+		videoTitle: "Java",
+		videoDescription: "General-purpose computer programming language that is concurrent, class-based, object-oriented and specifically designed to have as few implementation dependencies as possible",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/3u1fu6f8Hto" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "https://upload.wikimedia.org/wikipedia/en/8/88/Java_logo.png"
+	},
+	{
+		id: 5,
+		videoTitle: "C++",
+		videoDescription: "General-purpose programming language. It has imperative, object-oriented and generic programming features, while also providing facilities for low-level memory manipulation",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/Rub-JsjMhWY" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "http://static.jboss.org/rhd/images/images_products_multipath_cpp-logo-3.png"
+	},
+	{
+		id: 6,
+		videoTitle: "JavaScript",
+		videoDescription: "High-level, dynamic, untyped, and interpreted programming language",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/jXW5Z-MMYPA" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "http://www.mixed-up.com/markb/about/visual/js-logo-badge-512-edit.png"
+	},
+	{
+		id: 7,
+		videoTitle: "Python",
+		videoDescription: "High-level, general-purpose, interpreted, dynamic programming language",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/N4mEzFDjqtA" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "http://www.unixstickers.com/image/cache/data/stickers/python/python.sh-600x600.png"
+	},
+	{
+		id: 8,
+		videoTitle: "Ruby",
+		videoDescription: "Dynamic, reflective, object-oriented, general-purpose programming language",
+		videoIframe: '<div class="container"> <iframe class="video" src="https://www.youtube.com/embed/Dji9ALCgfpM" frameborder="0" allowfullscreen></iframe> </div>',
+		thumbnailUrl: "http://cache.soometa.com/image/257842_kiranatama.com"
 	}
 ];
 
